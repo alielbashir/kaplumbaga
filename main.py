2785cbba86c8197baf9b602806af7f53ac621eaf
Ali Elbashir <alielbashiir@gmail.com>
2023-03-07 20:01:33 +0300

    kare ciz

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ import turtle
 kaplumbaga = turtle.Turtle()
 
 kaplumbaga.forward(100)
+kaplumbaga.right(90)
+kaplumbaga.forward(100)
+kaplumbaga.right(90)
+kaplumbaga.forward(100)
+kaplumbaga.right(90)
+kaplumbaga.forward(100)
 
 turtle.exitonclick()
