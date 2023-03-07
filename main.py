0c8a99d1b62b76d0a5d0d52965439fd7cb0664de
Ali Elbashir <alielbashiir@gmail.com>
2023-03-07 20:02:58 +0300

    kare_cizi fonksiyona donustur

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import turtle
 
-kaplumbaga = turtle.Turtle()
+def kare_ciz():
+    kaplumbaga = turtle.Turtle()
+    for i in range(4):
+        kaplumbaga.forward(100)
+        kaplumbaga.right(90)
 
-kaplumbaga.forward(100)
-kaplumbaga.right(90)
-kaplumbaga.forward(100)
-kaplumbaga.right(90)
-kaplumbaga.forward(100)
-kaplumbaga.right(90)
-kaplumbaga.forward(100)
+
+kare_ciz()
 
 turtle.exitonclick()
